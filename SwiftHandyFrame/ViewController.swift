01ebffc762a9a274a374d0ff6bd6c916cb32a3f1
Mamba24 <akateason@qq.com>
2021-08-19 02:11:01 +0800

    [ADD] 链式调用

--- SwiftHandyFrame/ViewController.swift
+++ SwiftHandyFrame/ViewController.swift
@@ -24,36 +24,24 @@ class ViewController: UIViewController {
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        shapeView.hf.setSize(CGSize(width: 100, height: 100))
-        shapeView.hf.setLeft(20, shouldResize: false)
-        shapeView.hf.setBottom(200, shouldResize: true)
-        shapeView.hf.setTop(10, shouldResize: false)
+        shapeView.hf.setSize(CGSize(width: 100, height: 100)).setLeft(20, shouldResize: false).setBottom(200, shouldResize: true).setTop(10, shouldResize: false)
         
-        centerPositionWithOtherView.hf.setSize(CGSize(width: 50, height: 50))
-        centerPositionWithOtherView.hf.setCenterEqualToView(shapeView)
+        centerPositionWithOtherView.hf.setSize(CGSize(width: 50, height: 50)).setCenterEqualToView(shapeView)
         
         gapInOtherView.frame = centerPositionWithOtherView.frame
-        gapInOtherView.hf.setInnerTopGap(200, shouldResize: false)
-        gapInOtherView.hf.setInnerBottomGap(200, shouldResize: true)
+        gapInOtherView.hf.setInnerTopGap(200, shouldResize: false).setInnerBottomGap(200, shouldResize: true)
         
         gapFromOtherView.frame = gapInOtherView.frame
         gapFromOtherView.hf.setLeftGap(10, fromView: gapInOtherView)
         
         positionEqualOtherView.frame = gapFromOtherView.frame
-        positionEqualOtherView.hf.setInnerTopGap(500, shouldResize: true)
-        positionEqualOtherView.hf.bottomEqualToView(gapFromOtherView)
-        positionEqualOtherView.hf.setLeftGap(10, fromView: gapFromOtherView)
+        positionEqualOtherView.hf.setInnerTopGap(500, shouldResize: true).bottomEqualToView(gapFromOtherView).setLeftGap(10, fromView: gapFromOtherView)
         
-        fillView.hf.setSize(CGSize(width: 20, height: 20))
-        fillView.hf.fillWidth()
-        fillView.hf.setCenterYEqualToView(positionEqualOtherView)
+        fillView.hf.setSize(CGSize(width: 20, height: 20)).fillWidth().setCenterYEqualToView(positionEqualOtherView)
         
-        containerView.hf.setSize(CGSize(width: 100, height: 100))
-        containerView.hf.setCenterEqualToView(view)
+        containerView.hf.setSize(CGSize(width: 100, height: 100)).setCenterEqualToView(view)
 
-        containerInnerItemView.hf.setWidth(50)
-        containerInnerItemView.hf.setCenterEqualToView(containerView)
-        containerInnerItemView.hf.setInnerBottomGap(0, shouldResize: true)
+        containerInnerItemView.hf.setWidth(50).setCenterEqualToView(containerView).setInnerBottomGap(0, shouldResize: true)
     }
     
     lazy var containerView:UIView = {
